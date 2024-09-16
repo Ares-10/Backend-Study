2ce5accfd7bb7e9f30ef5b95abe25d711ad0f4a2
Ares-10 <cholionking1@naver.com>
2024-09-16 17:52:51 +0900

    feat: 게시글 작성하기

--- src/main/java/YOURSSU/assignment/service/user/UserService.java
+++ src/main/java/YOURSSU/assignment/service/user/UserService.java
@@ -1,8 +1,11 @@
 package YOURSSU.assignment.service.user;
 
+import YOURSSU.assignment.domain.User;
 import YOURSSU.assignment.dto.request.UserRequest.UserSignUpRequest;
 import YOURSSU.assignment.dto.response.UserResponse.UserSignUpResponse;
 
 public interface UserService {
     UserSignUpResponse signUp(UserSignUpRequest request);
+
+    User getUser(String email, String password);
 }
