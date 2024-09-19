79809491a73456f38a3a4d4bb2bc41932217b907
Ares-10 <cholionking1@naver.com>
2024-09-19 14:39:31 +0900

    fix: 로그인 시 유저 인증 방식 수정

--- src/main/java/YOURSSU/assignment/service/auth/AuthServiceImpl.java
+++ src/main/java/YOURSSU/assignment/service/auth/AuthServiceImpl.java
@@ -3,12 +3,11 @@ package YOURSSU.assignment.service.auth;
 import jakarta.transaction.Transactional;
 
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import YOURSSU.assignment.converter.AuthConverter;
+import YOURSSU.assignment.domain.User;
 import YOURSSU.assignment.dto.request.AuthRequest.LoginRequest;
 import YOURSSU.assignment.dto.request.AuthRequest.TokenRefreshRequest;
 import YOURSSU.assignment.dto.response.AuthResponse.LoginResponse;
@@ -16,6 +15,7 @@ import YOURSSU.assignment.dto.response.AuthResponse.TokenRefreshResponse;
 import YOURSSU.assignment.global.auth.provider.JwtTokenProvider;
 import YOURSSU.assignment.global.exception.GlobalErrorCode;
 import YOURSSU.assignment.global.exception.GlobalException;
+import YOURSSU.assignment.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -23,24 +23,18 @@ import lombok.RequiredArgsConstructor;
 public class AuthServiceImpl implements AuthService {
     private final JwtTokenProvider jwtTokenProvider;
     private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Override
     @Transactional
     public LoginResponse login(LoginRequest request) {
-        // 사용자 인증 처리 (이메일, 비밀번호)
-        Authentication authentication =
-                authenticationManager.authenticate(
-                        new UsernamePasswordAuthenticationToken(
-                                request.getEmail(), request.getPassword()));
-
-        // 인증된 사용자의 정보를 SecurityContext에 저장
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        // Access Token 및 Refresh Token 생성
+        // 비밀번호 검증
+        User user = userService.getUserByEmail(request.getEmail());
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword()))
+            throw new GlobalException(GlobalErrorCode.EMAIL_PASSWORD_MISMATCH);
         String accessToken = jwtTokenProvider.createAccessToken(request.getEmail());
         String refreshToken = jwtTokenProvider.createRefreshToken(request.getEmail());
-
-        // 응답 생성 및 반환
         return AuthConverter.toLoginResponse(accessToken, refreshToken, request.getEmail());
     }
 
@@ -50,9 +44,8 @@ public class AuthServiceImpl implements AuthService {
         String refreshToken = request.getRefreshToken();
 
         // Refresh Token 검증
-        if (!jwtTokenProvider.isValidToken(refreshToken, "REFRESH")) {
+        if (!jwtTokenProvider.isValidToken(refreshToken, "REFRESH"))
             throw new GlobalException(GlobalErrorCode.AUTH_INVALID_TOKEN);
-        }
 
         // Refresh Token에서 사용자 email 추출
         String email = jwtTokenProvider.getEmail(refreshToken);
